1ecd7c4d805daac7ea6eb2a24a53d962a91fc577
hbs1215 <hbs1215@naver.com>
2016-12-26 21:22:51 +0900

    test

---
+++ Untitled/Assets/Scripts/UI_Script/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Exit() {
+		#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+		#elif UNITY_WEBPLAYER
+			Application.OpenURL(webplayerQuitURL);
+		#else
+			Application.Quit();
+		#endif
+    }
+}
